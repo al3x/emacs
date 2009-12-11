7a22a0ddfb0103787f568d77361acd397431b2ab
Alex Payne <alex@twitter.com>
2009-12-10 16:32:05 -0800

    use auto-revert-mode everywhere all the time (thanks @stevej)

--- customizations/my-buffers.el
+++ customizations/my-buffers.el
@@ -60,3 +60,6 @@
 
 ; delete files by moving them to the OS X trash
 (setq delete-by-moving-to-trash t)
+
+; pick up changes to files on disk automatically (ie, after git pull)
+(global-auto-revert-mode 1)
