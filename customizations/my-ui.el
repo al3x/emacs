7ebb832bff7026042434259e54f5f7e6556b8caa
Alex Payne <al3x@al3x.net>
2009-12-13 20:27:00 -0800

    pretty diff mode colors

--- customizations/my-ui.el
+++ customizations/my-ui.el
@@ -32,3 +32,15 @@
 (custom-set-faces
  '(flymake-errline ((t :underline "red")))
  '(flymake-warnline ((t :underline "green"))))
+
+; pretty diff mode
+(custom-set-faces
+ '(diff-added ((t (:foreground "#559944"))))
+ '(diff-context ((t nil)))
+ '(diff-file-header ((((class color) (min-colors 88) (background dark)) (:foreground "RoyalBlue1"))))
+ '(diff-function ((t (:foreground "#00bbdd"))))
+ '(diff-header ((((class color) (min-colors 88) (background dark)) (:foreground "RoyalBlue1"))))
+ '(diff-hunk-header ((t (:foreground "#fbde2d"))))
+ '(diff-nonexistent ((t (:inherit diff-file-header :strike-through nil))))
+ '(diff-refine-change ((((class color) (min-colors 88) (background dark)) (:background "#182042"))))
+ '(diff-removed ((t (:foreground "#de1923")))))
