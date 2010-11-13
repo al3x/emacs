979f5aeb480fd327a5b09fb192f2f2adebd73bdd
Alex Payne <alex@twitter.com>
2010-11-12 21:28:40 -0800

    updating smart-tab mode, step 1

--- customizations/my-shell.el
+++ customizations/my-shell.el
@@ -2,26 +2,11 @@
 (setq path "/bin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/mysql/bin:/usr/local/google_appengine")
 (setenv "PATH" path)
 
-; more bash-like autocomplete
-(setq eshell-cmpl-cycle-completions nil)
-
-; automatically save history
-(setq eshell-save-history-on-exit t)
-
-; ignore version control directories when autocompleting
-(setq eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.git\\)/\\'")
-
 ; can't write over prompt, that would be weird
 (setq comint-prompt-read-only)
-
-; scroll to bottom on output, more like a terminal
-(setq eshell-scroll-to-bottom-on-output t)
-(setq eshell-scroll-show-maximum-output t)
 
 ; colorful shell
 (require 'ansi-color)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
-; escape the shell
-(add-hook 'eshell-mode-hook
-  '(lambda nil (local-set-key "\C-u" 'eshell-kill-input)))
+(setq ansi-term-color-vector [unspecified "#000000" "#963F3C" "#5FFB65" "#FFFD65" "#0082FF" "#FF2180" "#57DCDB" "#FFFFFF"])
