f50bf74a02544dc548b6db95405a39286bcb2671
Alex Payne <alex@twitter.com>
2010-11-17 13:44:58 -0800

    indent whole buffer utility function

---
+++ utilities/indent-whole-buffer.el
@@ -0,0 +1,6 @@
+(defun indent-whole-buffer ()
+  "indent whole buffer"
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
