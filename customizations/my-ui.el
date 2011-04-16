bc8ed49e7f7a7eb7b3ee8c1f76dcd0c2862b2def
Alex Payne <alex@banksimple.com>
2011-04-15 17:22:51 -0700

    switch to solarized (dark) for colors

--- customizations/my-ui.el
+++ customizations/my-ui.el
@@ -23,8 +23,7 @@
 (require 'color-theme)
 (color-theme-initialize)
 (setq color-theme-is-global t)
-(load-file "~/.emacs.d/vendor/twilight-emacs/color-theme-twilight.el")
-(color-theme-twilight)
+(color-theme-solarized-dark)
 
 ; colors
 (custom-set-faces
