9d9f355de7730041788f484ab1204699ca31180b
Alex Payne <al3x@al3x.net>
2014-05-29 23:14:46 -0700

    bring back some keyboard shortcuts from textmate-mode and remove some dead keys

--- mac-customizations/my-mac-keys.el
+++ mac-customizations/my-mac-keys.el
@@ -9,9 +9,6 @@
 ; switch to shell
 (global-set-key (kbd "s-0") 'visit-ansi-term)
 
-; search with ack
-(global-set-key (kbd "s-F") 'ack)
-
 ; open file
 (global-set-key [(super o)] 'find-file)
 
@@ -22,6 +19,25 @@
 ; frame switching
 (global-set-key (kbd "s-`") 'other-window)
 
+; indenting and outdenting
+(defun textmate-shift-right (&optional arg)
+  "Shift the line or region to the ARG places to the right.
+  A place is considered `tab-width' character columns."
+  (interactive)
+  (let ((deactivate-mark nil)
+        (beg (or (and mark-active (region-beginning))
+                 (line-beginning-position)))
+        (end (or (and mark-active (region-end)) (line-end-position))))
+    (indent-rigidly beg end (* (or arg 1) tab-width))))
+
+(defun textmate-shift-left (&optional arg)
+  "Shift the line or region to the ARG places to the left."
+  (interactive)
+  (textmate-shift-right (* -1 (or arg 1))))
+
+(global-set-key (kbd "s-[") 'textmate-shift-left)
+(global-set-key (kbd "s-]") 'textmate-shift-right)
+
 ; close window
 (global-set-key [(super w)] (lambda ()
   (interactive)
@@ -31,11 +47,3 @@
 ; navigating through errors
 (global-set-key [(meta n)] 'next-error)
 (global-set-key [(meta p)] 'previous-error)
-
-; run Ruby tests, TextMate-style
-(add-hook 'rinari-minor-mode-hook
-  (lambda ()
-    (define-key rinari-minor-mode-map (kbd "s-r") 'rinari-test)))
-
-; fullscreen!
-(global-set-key [(super return)] 'ns-toggle-fullscreen)
