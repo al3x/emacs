cd59ecb181a07ca3939f068ec72e7f9acee7ac08
Alex Payne <al3x@al3x.net>
2014-05-31 12:48:29 -0700

    smarter smart-mode-line settings

--- el-get-init/init-smart-mode-line.el
+++ el-get-init/init-smart-mode-line.el
@@ -1,4 +1,16 @@
 (setq sml/theme 'respectful)
-(sml/setup)
+
+(if after-init-time (sml/setup)
+  (add-hook 'after-init-hook 'sml/setup))
+
+(setq sml/shorten-directory t)
+(setq sml/shorten-modes t)
+(setq sml/name-width 25)
+(setq sml/mode-width 'full)
+
+(add-to-list 'sml/hidden-modes " SP")
+(add-to-list 'sml/hidden-modes " company")
+(add-to-list 'sml/hidden-modes " vl")
+(add-to-list 'sml/hidden-modes " Wrap")
 
 (add-to-list 'sml/replacer-regexp-list '("^~/src/dobt/" ":DOBT:"))
