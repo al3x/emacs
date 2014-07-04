74789d9b46a4f31bbf3d53a1e0be7edc8dfd67a3
Alex Payne <al3x@al3x.net>
2014-07-04 14:27:37 -0700

    mapc not mapcar

--- init.el
+++ init.el
@@ -39,10 +39,10 @@
 (load-file "~/.emacs.d/utilities/load-directory.el")
 
 ; load all utility functions
-(mapcar 'load-directory '("~/.emacs.d/utilities"))
+(mapc 'load-directory '("~/.emacs.d/utilities"))
 
 ; load personal customizations (keybindings, colors, etc.)
-(mapcar 'load-directory '("~/.emacs.d/customizations"))
+(mapc 'load-directory '("~/.emacs.d/customizations"))
 
 ; per-OS customizations
 (if (string-equal system-type "gnu/linux")
