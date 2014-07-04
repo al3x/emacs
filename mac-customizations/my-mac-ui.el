a17dc520cb251b281810fe41561542e9f8f6a00f
Alex Payne <al3x@al3x.net>
2014-07-03 18:06:56 -0700

    bigger font

--- mac-customizations/my-mac-ui.el
+++ mac-customizations/my-mac-ui.el
@@ -1,6 +1,6 @@
 ; nice font
 (defun fontify-frame (frame)
-  (set-frame-parameter frame 'font "Deja-Vu-Sans-Mono-15"))
+  (set-frame-parameter frame 'font "Deja-Vu-Sans-Mono-16"))
 
 ; fontify this and all future frames
 (fontify-frame nil)
