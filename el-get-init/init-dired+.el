f7abd7f9bd7225f56e99169fb521d9283e3260b7
Alex Payne <al3x@al3x.net>
2014-07-04 14:28:09 -0700

    spruce up dired

---
+++ el-get-init/init-dired+.el
@@ -0,0 +1,14 @@
+(toggle-diredp-find-file-reuse-dir 1)
+
+(defun mydired-sort ()
+  "Sort dired listings with directories first."
+  (save-excursion
+    (let (buffer-read-only)
+      (forward-line 2) ;; beyond dir. header
+      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+    (set-buffer-modified-p nil)))
+
+(defadvice dired-readin
+  (after dired-after-updating-hook first () activate)
+  "Sort dired listings with directories first before adding marks."
+  (mydired-sort))
