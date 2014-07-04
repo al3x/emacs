b1a802177016399c6bcbdfbe01639d02a5ad18f3
Alex Payne <al3x@al3x.net>
2014-07-04 15:34:34 -0700

    this just doesn't work

--- customizations/my-shell.el
+++ customizations/my-shell.el
@@ -4,5 +4,3 @@
 ; colorful shell
 (require 'ansi-color)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
-(setq ansi-term-color-vector [unspecified "#000000" "#963F3C" "#5FFB65" "#FFFD65" "#0082FF" "#FF2180" "#57DCDB" "#FFFFFF"])
