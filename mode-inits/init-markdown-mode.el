311a8f0cd99cb64a05058abd007b46fe1dac47c2
Alex Payne <al3x@al3x.net>
2014-09-12 14:21:26 -0700

    preview Markdown in Marked 2.app

--- mode-inits/init-markdown-mode.el
+++ mode-inits/init-markdown-mode.el
@@ -4,3 +4,13 @@
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mkd$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
+(defun markdown-preview-file ()
+  "run Marked on the current file and revert the buffer"
+  (interactive)
+  (shell-command
+   (format "open -a '/Applications/Marked 2.app' %s"
+           (shell-quote-argument (buffer-file-name))))
+  )
+
+(global-set-key "\C-cm" 'markdown-preview-file)
