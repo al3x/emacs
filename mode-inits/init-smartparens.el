c47bff244145a25c159d294d3b06eaea1cf2c928
Alex Payne <al3x@al3x.net>
2014-09-25 15:34:34 -0700

    be less "smart" in Markdown

--- mode-inits/init-smartparens.el
+++ mode-inits/init-smartparens.el
@@ -2,3 +2,6 @@
 (show-smartparens-global-mode +1)
 
 (sp-local-pair 'clojure-mode "'" nil :actions nil)
+
+(sp-local-pair 'markdown-mode "'" nil :actions nil)
+(sp-local-pair 'markdown-mode "\"" nil :actions nil)
