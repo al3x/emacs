69d74a7a3107ced3aa45f6949105ef2aa00b4815
Alex Payne <al3x@al3x.net>
2015-10-29 16:22:11 -0700

    support JSX and a better JS linter

--- mode-inits/init-webmode.el
+++ mode-inits/init-webmode.el
@@ -6,19 +6,13 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
 
 (defadvice web-mode-highlight-part (around tweak-jsx activate)
   (if (equal web-mode-content-type "jsx")
       (let ((web-mode-enable-part-face nil))
         ad-do-it)
     ad-do-it))
-
-(flycheck-define-checker jsxhint-checker
-  "A JSX syntax and style checker based on JSXHint."
-  :command ("jsxhint" source)
-  :error-patterns ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
-  :modes (web-mode))
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
@@ -32,11 +26,12 @@
   (auto-complete-mode 1)
   (lambda ()
     (when (equal web-mode-content-type "jsx")
-      ;; enable flycheck
-      (flycheck-select-checker 'jsxhint-checker)
+      (flycheck-select-checker 'javascript-eslint)
+      (setq-default flycheck-disabled-checkers
+                    (append flycheck-disabled-checkers
+                            '(json-jsonlist)))
       (flycheck-mode)))
   (add-hook 'web-mode-hook  'my-web-mode-hook))
 
-(autoload 'web-mode "web-mode" "web mode" t)
 
-(add-hook 'web-mode-hook (lambda () (tern-mode t)))
+(autoload 'web-mode "web-mode" "web mode" t)
