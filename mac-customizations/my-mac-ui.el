5bbc04894738cdcab93958ff9f78ec24015315e6
Alex Payne <al3x@al3x.net>
2015-10-29 16:22:17 -0700

    Use Hack font

--- mac-customizations/my-mac-ui.el
+++ mac-customizations/my-mac-ui.el
@@ -1,6 +1,6 @@
 ; nice font
 (defun fontify-frame (frame)
-  (set-frame-parameter frame 'font "Deja-Vu-Sans-Mono-16"))
+  (set-frame-parameter frame 'font "Hack-16"))
 
 ; fontify this and all future frames
 (fontify-frame nil)
